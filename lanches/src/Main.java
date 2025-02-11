c853738e40fc0b2b186c4e99e2142f9e3b8071f7
Natália <batistanatp@gmail.com>
2025-02-11 07:05:45 -0300

    Add files via upload

---
+++ lanches/src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int lanche1 = 10;
+        int lanche2 = 4;
+        int lanche3 = 2;
+
+        int total = lanche1 + lanche2 + lanche3;
+        float media = (float) total/3;
+
+        System.out.println("total de lanches: " + total);
+        System.out.println("Média de lanches:" + media);
+
+    }
+}
